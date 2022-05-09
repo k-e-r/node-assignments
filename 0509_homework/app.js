7f8a8b9b5672ce8d5d9c27c74aec270d9722f94f
k-e-r <damajonboua@gmail.com>
2022-05-09 12:13:46 -0700

    add homework

---
+++ 0509_homework/app.js
@@ -0,0 +1,33 @@
+// 2-b
+const mySum = require('./mySum');
+console.log(mySum(1, 2, 3, 1));
+
+// 2-c
+const myArr = [1, 2, 3];
+// 2-d
+const result = mySum(...myArr);
+console.log(result);
+
+// 2-e
+const mySecondArr = myArr.map((val) => val * 2);
+// 2-f
+const ave =
+  [...mySecondArr].reduce((sum, el) => sum + el, 0) / mySecondArr.length;
+console.log(mySecondArr.filter((val) => val >= ave));
+
+// 2-g
+setTimeout(() => console.log('Goodbye'), 3000);
+
+// 2-h
+const Employee = {
+  name: 'hoge',
+  email: 'test',
+  department: 'test',
+  startDate: 'today',
+};
+
+// 2-i
+const Person = new Object();
+Person.name = Employee.name;
+Person.email = Employee.email;
+console.log(Person);
---
+++ 0509_homework/mySum.js
@@ -0,0 +1,5 @@
+// 2-a
+const mySum = (...val) => val.reduce((sum, el) => sum + el, 0);
+
+// 2-b
+module.exports = mySum;
